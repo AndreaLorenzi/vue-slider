44367d5072476bd3c1f5a7ae39bf0fe53767a030
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-05-08 20:16:27 +0200

    add everythings

--- script.js
+++ script.js
@@ -1,36 +1,74 @@
 const app = Vue.createApp({
-    data(){
-        return{
-            images: [
-                {
-                    image: 'img/01.webp',
-                    title: 'Marvel\'s Spiderman Miles Morale',
-                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-                }, {
-                    image: 'img/02.webp',
-                    title: 'Ratchet & Clank: Rift Apart',
-                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-                }, {
-                    image: 'img/03.webp',
-                    title: 'Fortnite',
-                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-                }, {
-                    image: 'img/04.webp',
-                    title: 'Stray',
-                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-                }, {
-                    image: 'img/05.webp',
-                    title: "Marvel's Avengers",
-                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-                },
-            ],
-        };
-    },
-    methods: {
-
-    },
+	data() {
+		return {
+			arrImages: [
+				'img/01.webp',
+				'img/02.webp',
+				'img/03.webp',
+				'img/04.webp',
+				'img/05.webp',
+			],
+			activeIndex: 0,
+			interval: setInterval(this.next, 3000),
+			isActive: false
+		}
+	},
+	methods: {
+		selected(i){
+			this.activeIndex = i;
+		},
+		mouseOver() {
+			clearInterval(this.interval);
+		},
+		mouseLeave() {
+			this.interval = setInterval(this.next, 3000);
+		},
+		next() {
+			// settiamo il nuovo valore di active index
+			this.activeIndex++;
+			if (this.activeIndex >= this.arrImages.length) {
+				this.activeIndex = 0;
+			}
+			// caroselText.innerHTML = write(activeIndex);
+		},
+		prev() {
+			// settiamo il nuovo valore di active index
+			this.activeIndex--;
+			if (this.activeIndex < 0) {
+				this.activeIndex = this.arrImages.length - 1;
+			}
+			// caroselText.innerHTML = write(activeIndex);
+		},
+		write(num) {
+			let toReturn;
+			switch (num) {
+				case 0:
+					toReturn = "Spiderman"
+					break;
+				case 1:
+					toReturn = "Ratchet & Clank";
+					break;
+				case 2:
+					toReturn = "Fortnite";
+					break;
+				case 3:
+					toReturn = "Stray"
+					break;
+				case 4:
+					toReturn = "Avengers";
+					break;
+				default:
+					toReturn = "None"
+					break;
+			}
+			console.log(num)
+			return toReturn;
+		}
+	}
 });
-app.mount(".carousel")
+
+app.mount("#app");
+
 
 
 
